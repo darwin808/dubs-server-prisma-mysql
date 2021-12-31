55fffa333918fa9d7e160c5dd52d72bb1a91025d
darwin <darwin.apolinario@serino.com>
2021-12-31 16:32:27 +0800

    123

--- handlers/User/GetUsers.js
+++ handlers/User/GetUsers.js
@@ -1,17 +1,13 @@
 const { PrismaClient } = require("@prisma/client");
+const { headers } = require("../../constants/index");
 
 const prisma = new PrismaClient();
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Content-Type": "application/json",
-};
-
 exports.handler = async (event, context, callback) => {
   try {
     const users = await prisma.user.findMany();
     return {
       statusCode: 200,
-      headers,
+      headers: headers,
       body: JSON.stringify({ users }),
     };
   } catch (error) {
