9ea46fcbb7ef56dc8f2bea77cc48643a567e1776
darwin <darwin.apolinario@serino.com>
2021-12-31 14:27:43 +0800

    initial

--- handlers/Posts/GetPosts.js
+++ handlers/Posts/GetPosts.js
@@ -1,7 +1,11 @@
 // const { PrismaClient } = require("@prisma/client");
 
 // const prisma = new PrismaClient();
-
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Methods": "GET",
+};
 exports.handler = async (event, context, callback) => {
   try {
     // const posts = await prisma.post.findMany({
