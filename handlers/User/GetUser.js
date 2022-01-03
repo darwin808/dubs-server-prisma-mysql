320d8f9e1ac3f2e727a88c90c9416157a27ffc51
darwin <darwin.apolinario@serino.com>
2022-01-03 20:42:31 +0800

    update

---
+++ handlers/User/GetUser.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Methods": "GET",
+};
+exports.handler = async (event, context, callback) => {
+  const { id } = event.pathParameters;
+  try {
+    const users = await prisma.user.findUnique({
+      where: { id },
+      include: { posts: true, thread: true },
+    });
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ users }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify(error),
+    };
+  }
+};
