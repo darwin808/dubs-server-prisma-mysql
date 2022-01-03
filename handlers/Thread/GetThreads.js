bb5ec0aec7ccc59b990befb0225733ff78f297c6
darwin <darwin.apolinario@serino.com>
2022-01-03 12:25:09 +0800

    update

--- handlers/Thread/GetThreads.js
+++ handlers/Thread/GetThreads.js
@@ -9,6 +9,7 @@ const headers = {
 exports.handler = async (event, context, callback) => {
   const { id } = event.pathParameters;
   const q1 = event.queryStringParameters;
+  const { page } = event.queryStringParameters || 1;
   try {
     const totalItems = await prisma.thread.findMany();
     const thread = await prisma.thread.findMany({
@@ -21,7 +22,7 @@ exports.handler = async (event, context, callback) => {
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify({ thread, totalItems: totalItems.length, q1 }),
+      body: JSON.stringify({ thread, totalItems: totalItems.length, q1, page }),
     };
   } catch (error) {
     console.error(error);
