22dfd081a63c58775e0241a1ed741b61f6fedec1
darwin <darwin.apolinario@serino.com>
2022-01-09 15:30:11 +0800

    redis

--- handlers/Thread/GetThreads.js
+++ handlers/Thread/GetThreads.js
@@ -2,8 +2,9 @@ const { PrismaClient } = require("@prisma/client");
 const { createClient } = require("redis");
 
 const redis_url = `redis://:12345678@${process.env.REDIS_HOSTNAME}:17028`;
+
 const prisma = new PrismaClient();
-const redis = redis.createClient({
+const redis = createClient({
   URL: redis_url,
 });
 
