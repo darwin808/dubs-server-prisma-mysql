809052d40dd0cc055c161bfc5475340ad5bc675b
darwin <darwin.apolinario@serino.com>
2022-01-18 21:18:52 +0800

    update routes

--- handlers/Admin/Register.js
+++ handlers/Admin/Register.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 const prisma = new PrismaClient();
 
 const headers = {
@@ -9,33 +9,34 @@ const headers = {
 };
 exports.handler = async (event, context, callback) => {
   const { email, password, username } = event.body;
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(password, salt);
+  // const salt = await bcrypt.genSalt(10);
+  // const hashPassword = await bcrypt.hash(password, salt);
 
   try {
-    const isEmailExist = await prisma.user.findUnique({ email });
-    if (isEmailExist) {
-      return {
-        statusCode: 400,
-        headers,
-        body: JSON.stringify({ error: "Email already registered" }),
-      };
-    }
+    console.log(email, password.username);
+    // const isEmailExist = await prisma.user.findUnique({ where: { email } });
+    // if (isEmailExist) {
+    //   return {
+    //     statusCode: 400,
+    //     headers,
+    //     body: JSON.stringify({ error: "Email already registered" }),
+    //   };
+    // }
     const newUser = await prisma.user.create({
       data: {
-        username,
-        email,
-        password: hashPassword,
+        username: "123",
+        email: "123",
+        password: "123",
       },
     });
 
-    const { password, ...data } = await newUser.toJSON();
+    // const { password, ...data } = await newUser.toJSON();
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify({ success: "Success", data }),
+      body: JSON.stringify({ success: "Success", newUser }),
     };
   } catch (error) {
-    return res.send({ error });
+    return { statusCode: 400, headers, body: JSON.stringify({ error }) };
   }
 };
